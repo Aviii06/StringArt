f3b7d9fc9e619788487c798f7fc18adaee0ab1c2
Archit Gosain <architgosain0601@gmail.com>
2023-09-14 14:48:10 +0530

    WIP

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #define PI 3.14159265f
 #define TOTAL_POINTS 100
 const int fidelity = 100;
-const int scale = 4;
+const int scale = 8;
 
 String fileNameArg;
 std::vector<float> g_Pixels;
@@ -16,11 +16,11 @@ void ImageToPixels(String fileName)
     image.resize(Magick::Geometry(fidelity, fidelity));
 
     image.write(&blob, "RGB");
-    for (int i = 0; i < fidelity; i += 1)
+    for (int i = fidelity - 1; i >= 0; i--)
     {
-        for (int j = 0; j < fidelity; j += 1)
+        for (int j = 0; j < fidelity; j++)
         {
-            Magick::ColorRGB px = image.pixelColor(i, j);
+            Magick::ColorRGB px = image.pixelColor(j, i);
             float col = (px.red() + px.green() + px.blue()) / 3;
             g_Pixels.push_back(col * 255 + 1.0f);
         }
@@ -31,6 +31,13 @@ void ImageToPixels(String fileName)
 //    {
 //        g_Pixels.push_back(((float)str[i] + (float)str[i + 1] + (float)str[i + 2]) / 3.0f);
 //    }
+}
+
+// If x == 0 -> function output = 1
+// Function decreases as x increases
+float SmoothnessFunction(float x)
+{
+    return 2 / (1 + exp(x * 20));
 }
 
 void LinesToPixel(std::vector<float>& pixels, int totalPoints)
@@ -49,18 +56,27 @@ void LinesToPixel(std::vector<float>& pixels, int totalPoints)
 
         float m = (point2.y - point1.y) / (point2.x - point1.x);
         float c = point1.y - m * point1.x;
-        for (int y = 0; y < fidelity; y++)
+        for (int j = 0; j < fidelity; j++)
         {
-            for (int x = 0; x < fidelity; x++)
+            for (int i = 0; i < fidelity; i++)
             {
-                float pixelValue = (pixels)[y * fidelity + x];
+                float pixelValue = (pixels)[j * fidelity + i];
+
+                float x = 2 * ((float)i - fidelity /2) / fidelity;
+                float y = 2 * ((float)j - fidelity /2) / fidelity;
 
                 float dist = abs(m * x + c - y) / sqrt(m * m + 1);
-                float addValue = 255 * exp(-dist * dist / 2.0f) + 1.0f;
+                float addValue = 0.0f;
+
+                float thickness = 0.25;
+                if (dist < thickness)
+                {
+                  addValue = 255 * std::min(1.0f, SmoothnessFunction(dist));
+                }
 
                 pixelValue = std::min(256.0f, pixelValue + addValue);
 
-                (pixels)[y * fidelity + x] = pixelValue;
+                (pixels)[j * fidelity + i] = pixelValue;
             }
         }
     }
@@ -88,13 +104,14 @@ void ProgressBar(int curr, int total)
 
 class ExampleInterface : public RenderingInterface
 {
+private:
+  std::vector<float> temp_pixels;
 public:
     void Setup() override
     {
         OPENGL_2D_CONFS
 
         // Figure out the lines
-        std::vector<float> temp_pixels;
         temp_pixels.reserve(fidelity * fidelity);
         for (int i = 0; i < fidelity * fidelity; i++)
         {
@@ -123,12 +140,13 @@ public:
                 g_Lines.push_back({j, i});
                 LinesToPixel(temp_pixels, TOTAL_POINTS);
 
+
                 // Find distance between pixels
                 float distance = 0.0f;
                 for (int y = 0; y < fidelity; y++) {
                     for (int x = 0; x < fidelity; x++) {
-                        distance += ((temp_pixels)[y * fidelity + x] - g_Pixels[y * fidelity + x]) *
-                                    ((temp_pixels)[y * fidelity + x] - g_Pixels[y * fidelity + x]);
+                        distance += ((256.0f - (temp_pixels)[y * fidelity + x]) - g_Pixels[y * fidelity + x]) *
+                                    ((256.0f - (temp_pixels)[y * fidelity + x]) - g_Pixels[y * fidelity + x]);
                     }
                 }
 
@@ -147,16 +165,36 @@ public:
             ProgressBar(count, TOTAL_POINTS - 1);
         }
 
+        LinesToPixel(temp_pixels, TOTAL_POINTS);
+
         Vivid::Renderer2D::Init();
     }
 
     void Draw() override
     {
+
+
+        Vivid::Renderer2D::BeginScene();
+
+//         draw the pixels
+//        for (int y = 0; y < fidelity; y++)
+//        {
+//            for (int x = 0; x < fidelity; x++)
+//            {
+//                float pixelValue = (temp_pixels)[y * fidelity + x];
+//                float color = 1.0 - pixelValue / 256.0f;
+//
+//                Vivid::Renderer2D::DrawQuad((x - fidelity/2) * scale, (y - fidelity/2) * scale, scale, scale, Vec3(color, color, color));
+//            }
+//        }
+
+        Vivid::Renderer2D::EndScene();
+
         Vivid::Renderer2D::BeginScene();
 
         // Draw using API
-        Vivid::Renderer2D::DrawQuad(0, 0, 1920*2, 1080*2, Vec3(0.0f, 0.0f, 0.0f));
-        Vivid::Renderer2D::DrawCircle(Vec2(0, 0), fidelity * scale, Vec3(1.0f, 1.0f, 1.0f));
+        //        Vivid::Renderer2D::DrawQuad(0, 0, 1920*2, 1080*2, Vec3(0.0f, 0.0f, 0.0f));
+        //        Vivid::Renderer2D::DrawCircle(Vec2(0, 0), fidelity * scale / 2.0f, Vec3(1.0f, 1.0f, 1.0f));
 
         // draw the lines
         for (auto line : g_Lines)
@@ -166,27 +204,12 @@ public:
             Vec2 point1 = {cos(angle1), sin(angle1)};
             Vec2 point2 = {cos(angle2), sin(angle2)};
 
-            Vivid::Renderer2D::DrawLine(Vec2(point1.x * fidelity * scale, point1.y * fidelity * scale),
-                                        Vec2(point2.x * fidelity * scale, point2.y * fidelity * scale),
+            Vivid::Renderer2D::DrawLine(Vec2(point1.x * fidelity * scale/2 , point1.y * fidelity * scale/2),
+                                        Vec2(point2.x * fidelity * scale/2, point2.y * fidelity * scale/2),
                                         1.0f,
-                                        Vec3(0.0f, 0.0f, 0.0f));
+                                        Vec3(1.0f, 0.0f, 0.0f));
         }
 
-
-        Vivid::Renderer2D::EndScene();
-
-        Vivid::Renderer2D::BeginScene();
-
-        // draw the pixels
-//        for (int y = 0; y < fidelity; y++)
-//        {
-//            for (int x = 0; x < fidelity; x++)
-//            {
-//                float pixelValue = (g_Pixels)[x * fidelity + y];
-//                float color = pixelValue / 256.0f;
-//                Vivid::Renderer2D::DrawQuad((-x + fidelity/2) * scale, (-y + fidelity/2) * scale, scale, scale, Vec3(color, color, color));
-//            }
-//        }
 
         Vivid::Renderer2D::EndScene();
     }
